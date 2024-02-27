80b4586997ded8df111f7352679648ac7b801a7d
Moises Yacila Palacios <myacilapalacios@horizon.csueastbay.edu>
2024-02-27 15:40:23 -0800

    Fixed issue adding players to the DB

--- mmpokervite/src/pages/newplayer.jsx
+++ mmpokervite/src/pages/newplayer.jsx
@@ -2,35 +2,57 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import axios from 'axios';
+import { useState } from 'react';
 
 export default function NewPlayer() {
-    const postPlayer = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    //Submit handler
+    const postPlayer = (e) => {
+        //Prevent redirect to server side response: res.send(req.body)
+        e.preventDefault();
+
+        //Save full name in a variable, if we use useState for the name, it won't get the last character
+        const finalName = `${firstName} ${lastName}`
+        //Send post request for express to handle and clear the inputs of the form
         axios.post('http://localhost:8080/players', {
-            firstName: document.getElementById('first-name').value,
-            lastName: document.getElementById('last-name').value
+            name: finalName
         }).then((response) => {
-            console.log(response);
-        })
+            console.log(response)
+            setFirstName('');
+            setLastName('');
+        }).catch(function (error) {
+            console.log(error);
+        });
     }
     return (
         <Box>
             <h1>Add New Player</h1>
-            <Box component="form" action='http://localhost:8080/players' method='POST' autoComplete='off' sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginTop: '2rem'
-            }}>
-                <TextField id="first-name"
+            <Box component="form"
+                action='http://localhost:8080/players' method='POST'
+                onSubmit={postPlayer} sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    marginTop: '2rem'
+                }}>
+                <TextField id="first"
                     label="First Name"
-                    variant="outlined" name='first-name'
-                    sx={{ width: '30%', marginBottom: '1rem' }} />
-                <TextField id="last-name"
+                    variant="outlined" name='first' value={firstName}
+                    onChange={(e) => {
+                        setFirstName(e.target.value);
+                    }}
+                    sx={{ width: '30%', marginBottom: '1rem' }} required />
+                <TextField id="last"
                     label="Last Name"
-                    variant="outlined" name='last-name'
-                    sx={{ width: '30%', marginBottom: '1rem' }} />
-                <Button variant="contained" size='large' type='submit' onClick={() => { postPlayer() }}>Add Player</Button>
+                    variant="outlined" name='last' value={lastName}
+                    onChange={(e) => {
+                        setLastName(e.target.value);
+                    }}
+                    sx={{ width: '30%', marginBottom: '1rem' }} required />
+                <Button variant="contained" size='large' type='submit'>Add Player</Button>
             </Box>
         </Box>
 
--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const app = express();
+const Player = require('./models/player');
+const bodyParser = require('body-parser');
 
 //Connect to DB
 mongoose.connect('mongodb://127.0.0.1:27017/mmpoker')
@@ -13,14 +15,23 @@ mongoose.connect('mongodb://127.0.0.1:27017/mmpoker')
         console.log(err)
     });
 
+//Check documentation if there are any questions with these
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('MoisoGenio');
 })
 
+//Post request handling adding players to DB
 app.post('/players', async (req, res) => {
+    console.log(req.body)
+    const name = req.body.name;
+    const player = new Player({ name: name });
+    await player.save();
+    //Check if this is necessary.
+    //e.preventDefault() on the form submit handler avoids getting to this page
     res.send(req.body);
 })
 
