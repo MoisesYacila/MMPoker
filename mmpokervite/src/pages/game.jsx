3c7d6b6e24e72ef7b3477f69d59eecc60a5cbe1c
Moises Yacila Palacios <moises.yacila.cs@gmail.com>
2025-02-06 16:18:14 +0100

    small button adds, first commit back

--- mmpokervite/src/pages/game.jsx
+++ mmpokervite/src/pages/game.jsx
@@ -1,4 +1,4 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import Box from '@mui/material/Box';
@@ -8,11 +8,17 @@ import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
+import Button from '@mui/material/Button';
+import ModeEditIcon from '@mui/icons-material/ModeEdit';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function Game() {
     //useLocation helps retrieve the data we passed in the navigate function that took us to this page
     const location = useLocation();
     const gameData = location.state.gameData;
+    
+    //Activate navigate
+    const navigate = useNavigate();
 
     //date is a string in the gameData object, so we create a Date object to work with it more efficiently
     const gameDate = new Date(gameData.date);
@@ -42,9 +48,10 @@ export default function Game() {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             <h1>Home Game</h1>
-            <h2>{`${gameDate.getFullYear()}/${gameDate.getMonth()}/${gameDate.getDate()}`}</h2>
+            <h2>{`${gameDate.getFullYear()}/${gameDate.getMonth() + 1}/${gameDate.getDate()}`}</h2>
+            <h3>Prize Pool: ${gameData.prizePool}</h3>
 
-            <TableContainer sx={{ width: '75%' }}>
+            <TableContainer sx={{ width: '75%', marginTop: '1rem', marginBottom: '2rem' }}>
                 <Table>
                     <TableHead>
                         <TableRow>
@@ -59,11 +66,22 @@ export default function Game() {
                     </TableHead>
                     <TableBody>
                         {
+                            // Similar logic as on the leaderboard
                             gameData.leaderboard.map((player, i) => {
                                 return (
                                     <TableRow key={i}>
                                         <TableCell>{i + 1}</TableCell>
-                                        <TableCell>{playerName[i]}</TableCell>
+                                        <TableCell>
+                                            {/* player is an object, so we need to access the player.player to get the id, then send data to navigate function */}
+                                            <Button sx={{textTransform:'none'}} onClick={async() => {
+                                                const link = `/players/${player.player}`;
+                                                await axios.get(`http://localhost:8080/players/${player.player}`)
+                                                .then((res) => {
+                                                    navigate(link, { state: { playerData: res.data } });
+                                                });
+                                            }}>{playerName[i]}</Button>
+                                            
+                                            </TableCell>
                                         <TableCell align='center'>{player.profit}</TableCell>
                                         <TableCell align='center'>{player.rebuys}</TableCell>
                                         <TableCell align='center'>{player.addOns}</TableCell>
@@ -75,6 +93,12 @@ export default function Game() {
                     </TableBody>
                 </Table>
             </TableContainer>
+
+            <Box sx={{display: 'flex', justifyContent: 'space-evenly', marginBottom: '2rem', width: '15%'}}>
+                <Button variant='contained' color='success' endIcon={<ModeEditIcon/>}>Edit</Button>
+                <Button variant='contained' color='error'endIcon={<DeleteIcon/>}>Delete</Button>
+            </Box>
+
         </Box>
     )
 }
