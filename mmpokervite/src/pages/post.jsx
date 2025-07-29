d14374941fe394312fd19ffc952be45b42c93b1f
Moises Yacila Palacios <moises.yacila.cs@gmail.com>
2025-07-29 14:38:10 +0200

    added markup for comment's text area

--- mmpokervite/src/pages/post.jsx
+++ mmpokervite/src/pages/post.jsx
@@ -1,7 +1,7 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Box, CircularProgress, IconButton } from "@mui/material";
+import { Box, Button, CircularProgress, IconButton, TextareaAutosize } from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import AddCommentIcon from '@mui/icons-material/AddComment';
 import { useUser } from "../UserContext";
@@ -12,6 +12,7 @@ export default function Post() {
     const [name, setName] = useState('');
     const { id: userId } = useUser();
     const [isLiked, setIsLiked] = useState(false);
+    const [textFieldActive, setTextFieldActive] = useState(false);
 
     // Load post data on the first render
     // If postData is not set, fetch it from the server
@@ -55,40 +56,61 @@ export default function Post() {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
             {postData ? (
-                <Box sx={{ padding: '2rem', textAlign: 'center', width: '80%' }}>
-                    {/* Display post title and content */}
-                    <h1>{postData.title}</h1>
-                    <h4>Posted by {name} on {new Date(postData.date).toLocaleDateString()}</h4>
-                    {postData.image && (
-                        <img src={postData.image} alt="Post" style={{ width: '15%' }} />
-                    )}
-                    <p>{postData.content} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad pariatur, adipisci similique totam quidem maiores iusto animi? Quasi tenetur, quibusdam illo vitae dolor voluptas! Quidem asperiores nobis adipisci eius tempora.
-                        Doloremque amet ullam ea molestiae, nisi id magnam inventore, eius consequuntur ipsum modi. Facere asperiores culpa ut fugit? Minima, eveniet nemo. Suscipit, nemo laborum aspernatur voluptas in voluptatum. Ducimus, dolor.
-                        Aliquid molestiae officiis unde labore odio velit, corrupti nam dicta excepturi minima, consequatur laboriosam necessitatibus exercitationem tempora optio, expedita fugiat voluptate in? Quas numquam dolor enim maxime facere vero aperiam.
-                        Iure, provident repudiandae ipsa delectus sunt cupiditate deserunt consequatur quibusdam harum asperiores repellat, accusantium quod fugiat totam. Porro tenetur magnam doloribus exercitationem, officiis in voluptatibus fuga? A nihil iste modi?
-                        Eos dolor excepturi maiores odit ad. Nulla numquam voluptatibus omnis. Aliquam qui cupiditate magnam, dicta odio iusto voluptatibus modi, eveniet obcaecati provident sed, praesentium quos. Tempore, saepe enim! Officiis, natus.</p>
-                    {/* Show the button red if the user has already liked it and gray otherwise. Disable the button if the user is not logged in */}
-                    <IconButton color={isLiked ? 'error' : 'default'} disabled={userId ? false : true} onClick={() => {
-                        // Handle like action
-                        // The empty object is the data we are sending, which is empty in this case
-                        // The withCredentials option is set to true to include cookies in the request
-                        // If we send credentials without the empty object, the server will not recognize the request
-                        axios.patch(`http://localhost:8080/posts/${id}/like`, {}, { withCredentials: true })
-                            .then((res) => {
-                                setPostData(res.data);
-                            })
-                            .catch(() => {
-                                console.error('Error liking post');
-                            });
-                    }}>
-                        <FavoriteIcon />
-                    </IconButton>
-                    {postData.likes || 0}
-                    <IconButton>
-                        <AddCommentIcon />
-                    </IconButton>
+                <>
+                    <Box sx={{ padding: '2rem', textAlign: 'center', width: '80%' }}>
+                        {/* Display post title and content */}
+                        <h1>{postData.title}</h1>
+                        <h4>Posted by {name} on {new Date(postData.date).toLocaleDateString()}</h4>
+                        {postData.image && (
+                            <img src={postData.image} alt="Post" style={{ width: '15%' }} />
+                        )}
+                        <p>{postData.content} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ad pariatur, adipisci similique totam quidem maiores iusto animi? Quasi tenetur, quibusdam illo vitae dolor voluptas! Quidem asperiores nobis adipisci eius tempora.
+                            Doloremque amet ullam ea molestiae, nisi id magnam inventore, eius consequuntur ipsum modi. Facere asperiores culpa ut fugit? Minima, eveniet nemo. Suscipit, nemo laborum aspernatur voluptas in voluptatum. Ducimus, dolor.
+                            Aliquid molestiae officiis unde labore odio velit, corrupti nam dicta excepturi minima, consequatur laboriosam necessitatibus exercitationem tempora optio, expedita fugiat voluptate in? Quas numquam dolor enim maxime facere vero aperiam.
+                            Iure, provident repudiandae ipsa delectus sunt cupiditate deserunt consequatur quibusdam harum asperiores repellat, accusantium quod fugiat totam. Porro tenetur magnam doloribus exercitationem, officiis in voluptatibus fuga? A nihil iste modi?
+                            Eos dolor excepturi maiores odit ad. Nulla numquam voluptatibus omnis. Aliquam qui cupiditate magnam, dicta odio iusto voluptatibus modi, eveniet obcaecati provident sed, praesentium quos. Tempore, saepe enim! Officiis, natus.</p>
+                        {/* Show the button red if the user has already liked it and gray otherwise. Disable the button if the user is not logged in */}
+                        <IconButton color={isLiked ? 'error' : 'default'} disabled={userId ? false : true} onClick={() => {
+                            // Handle like action
+                            // The empty object is the data we are sending, which is empty in this case
+                            // The withCredentials option is set to true to include cookies in the request
+                            // If we send credentials without the empty object, the server will not recognize the request
+                            axios.patch(`http://localhost:8080/posts/${id}/like`, {}, { withCredentials: true })
+                                .then((res) => {
+                                    setPostData(res.data);
+                                })
+                                .catch(() => {
+                                    console.error('Error liking post');
+                                });
+                        }}>
+                            <FavoriteIcon />
+                        </IconButton>
+                        {postData.likes || 0}
+                        <IconButton onClick={() => {
+                            // Toggle the text field for adding a comment
+                            setTextFieldActive(!textFieldActive);
+                        }
+                        }>
+                            <AddCommentIcon />
+                        </IconButton>
+                    </Box>
+                    <TextareaAutosize name='comment' minRows={3} placeholder='Add your comment here...'
+                        style={{ width: '30%', marginBottom: '1rem', padding: '10px', fontSize: '16px', backgroundColor: '#f5f3f4', display: textFieldActive ? 'block' : 'none' }}>
+                    </TextareaAutosize>
+                    <Box>
+                        <Button sx={{ display: textFieldActive ? 'inline' : 'none' }}
+                            onClick={() => {
+                                // Handle comment submission
+                                console.log('Comment submitted');
+                                setTextFieldActive(false);
+                            }}
+                        >Comment</Button>
+                        <Button color='error' sx={{ display: textFieldActive ? 'inline' : 'none' }} onClick={() => {
+                            setTextFieldActive(false);
+                        }}>Cancel</Button>
+                    </Box>
 
-                </Box>
+                </>
             ) : (
                 <CircularProgress />
             )}
