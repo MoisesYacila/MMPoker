aa806b3abec7369bbee03b12376491b5104e856c
Moises Yacila <moises.yacila.cs@gmail.com>
2025-12-06 00:23:44 +0100

    implemented responsive mobile navigation with hamburger menu

--- mmpokervite/src/components/Navigation.jsx
+++ mmpokervite/src/components/Navigation.jsx
@@ -5,9 +5,10 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { IconButton, Menu, MenuItem } from '@mui/material';
+import { Collapse, IconButton, List, ListItem, ListItemButton, ListItemText, Menu, MenuItem } from '@mui/material';
 import { styled } from '@mui/material/styles';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import MenuIcon from '@mui/icons-material/Menu';
 import { Link, useNavigate } from 'react-router-dom';
 import './navigation.css';
 import { useUser } from '../UserContext';
@@ -26,6 +27,7 @@ export default function Navigation() {
     const navigate = useNavigate();
 
     const [menuAnchor, setMenuAnchor] = useState(null);
+    const [openDrawer, setOpenDrawer] = useState(false);
 
     const handleMenuOpen = (e) => {
         setMenuAnchor(e.currentTarget);
@@ -51,12 +53,17 @@ export default function Navigation() {
     }
 
     return (
-        <Box className='navigation'>
-            <AppBar position='sticky' sx={{ display: 'flex', justifyContent: 'space-around' }}>
-                <Toolbar>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1, justifyContent: 'space-around' }}>
-                        <Link to='/' onClick={() => { closeAlert() }}>MMPoker League Manager</Link>
-                    </Typography>
+        <AppBar className='navigation' position='sticky' sx={{ display: 'flex', justifyContent: 'space-around' }}>
+            <Toolbar>
+                <Typography variant="h6" component="div" sx={{ flexGrow: 1, justifyContent: 'space-around' }}>
+                    <Link to='/' onClick={() => {
+                        closeAlert();
+                        setOpenDrawer(false);
+                    }}>MMPoker League Manager</Link>
+                </Typography>
+
+                {/* Desktop navbar will show on screen sizes md and larger */}
+                <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
                     <LinkButton>
                         <Typography><Link to='/leaderboard' onClick={() => { closeAlert() }}>Leaderboard</Link></Typography>
                     </LinkButton>
@@ -81,11 +88,97 @@ export default function Navigation() {
                                     <Typography><Link to='/login' onClick={() => { closeAlert() }}>Log in</Link></Typography>
                                 </LinkButton>)
                     }
-                </Toolbar>
-            </AppBar>
-            {/* Menu for logged in users to show their profile and logout */}
+                </Box>
+                {/* Hamburger icon for the menu will show on screens smaller than md size */}
+                <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+                    <IconButton color="inherit" onClick={() => setOpenDrawer(!openDrawer)}>
+                        <MenuIcon />
+                    </IconButton>
+                </Box>
+            </Toolbar>
+
+            {/* Mobile menu content will show on screens smaller than md size*/}
+            <Collapse in={openDrawer} timeout={{ enter: 450, exit: 450 }} unmountOnExit>
+                <List sx={{ display: { xs: 'block', md: 'none' } }}>
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton onClick={() => {
+                            closeAlert();
+                            setOpenDrawer(false);
+                            navigate('/leaderboard');
+                        }}>
+                            <ListItemText>Leaderboard</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton onClick={() => {
+                            closeAlert();
+                            setOpenDrawer(false);
+                            navigate('/players');
+                        }}>
+                            <ListItemText>Players</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton onClick={() => {
+                            closeAlert();
+                            setOpenDrawer(false);
+                            navigate('/updates');
+                        }}>
+                            <ListItemText>Updates</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton onClick={() => {
+                            closeAlert();
+                            setOpenDrawer(false);
+                            navigate('/stats');
+                        }}>
+                            <ListItemText>Stats</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                    {/* The next 3 items will show or not depending on the login status */}
+                    {/* Show if logged in */}
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton
+                            sx={{ display: loggedIn ? 'block' : 'none' }}
+                            onClick={() => {
+                                closeAlert();
+                                setOpenDrawer(false);
+                                navigate('/account');
+                            }}>
+                            <ListItemText>Account</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                    {/* Show if logged in */}
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton
+                            sx={{ display: loggedIn ? 'block' : 'none' }}
+                            onClick={() => {
+                                closeAlert();
+                                setOpenDrawer(false);
+                                handleLogout();
+                            }}>
+                            <ListItemText>Log out</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                    {/* Show if not logged in */}
+                    <ListItem sx={{ display: 'block' }} disablePadding>
+                        <ListItemButton
+                            sx={{ display: loggedIn ? 'none' : 'block' }}
+                            onClick={() => {
+                                closeAlert();
+                                setOpenDrawer(false);
+                                navigate('/login');
+                            }}>
+                            <ListItemText>Log in</ListItemText>
+                        </ListItemButton>
+                    </ListItem>
+                </List>
+            </Collapse>
+
+            {/* Menu for logged in users to show their profile and logout. It will only show on screen sizes larger than md */}
             {/* MUI Syntax */}
-            <Menu anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={() => setMenuAnchor(null)}>
+            <Menu sx={{ display: { xs: 'none', md: 'flex' } }} anchorEl={menuAnchor} open={Boolean(menuAnchor)} onClose={() => setMenuAnchor(null)}>
                 <MenuItem onClick={() => {
                     closeAlert();
                     setMenuAnchor(null);
@@ -97,6 +190,6 @@ export default function Navigation() {
                     handleLogout();
                 }}>Log out</MenuItem>
             </Menu>
-        </Box>
+        </AppBar>
     );
 }
